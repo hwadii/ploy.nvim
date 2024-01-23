4ad4569456fa05722bc9cc19670d615069effd02
Wadii Hajji <wadii@cardiologs.com>
2024-01-23 09:23:27 +0100

    fix: fix symbol colors

--- lua/ploy.lua
+++ lua/ploy.lua
@@ -218,7 +218,7 @@ local theme = lush(function(injected_functions)
     sym("@string.regexp")                { Constant },
     sym("@string.escape")                { Special },
     sym("@string.special")               { Special },
-    sym("@string.special.symbol")        { Identifier },
+    sym("@string.special.symbol")        { fg = colors.bright_green },
     sym("@string.special.url")           { sym "@string.special" },
     sym("@string.special.path")          { sym "@string.special" },
 
@@ -289,7 +289,7 @@ local theme = lush(function(injected_functions)
     sym("@markup.environment")           { PreProc },
 
     sym("@markup.link")                  { Constant },
-    sym("@markup.link.label")            { Special },
+    sym("@markup.link.label")            { Normal },
     sym("@markup.link.url")              { Constant, gui = "underline" },
 
     sym("@markup.raw")                   { Constant },
