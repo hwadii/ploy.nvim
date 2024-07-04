fdf6f053c74fb4409fff9b6f3ff5b5455f2290fe
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-07-04 14:20:51 +0200

    feat: lighter orange for types

--- lua/ploy.lua
+++ lua/ploy.lua
@@ -137,7 +137,7 @@ local theme = lush(function(injected_functions)
     -- Macro          { }, --   Same as Define
     -- PreCondit      { }, --   Preprocessor #if, #else, #endif, etc.
 
-    Type           { fg = colors.yellow.da(10) }, -- (*) int, long, char, etc.
+    Type           { fg = colors.yellow }, -- (*) int, long, char, etc.
     -- StorageClass   { }, --   static, register, volatile, etc.
     -- Structure      { }, --   struct, union, enum, etc.
     -- Typedef        { }, --   A typedef
